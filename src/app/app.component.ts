cdc19496987d69f0c8170347fbeb51c308742d52
hasanHelvali <114858957+hasanHelvali@users.noreply.github.com>
2023-10-21 17:54:15 +0300

    ApidenIstekAlma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,35 +13,10 @@ declare var $:any;
 export class AppComponent {
   title = 'ECommerceClient';
 
-  /**
-   *
-   */
-  constructor(private toastr:CustomToastrService) {
-
-  }
 
 
-  m(){
-    // this.toastr.message("Hasan Helvali","Selamun Aleykum",ToastrMessageType.Info,ToastrPosition.BottomCenter);
-    // this.toastr.message("Hasan Helvali","Selamun Aleykum",ToastrMessageType.Warning,ToastrPosition.BottomFullWidth);
-    // this.toastr.message("Hasan Helvali","Selamun Aleykum",ToastrMessageType.Error,ToastrPosition.BottomLeft);
-    // this.toastr.message("Hasan Helvali","Selamun Aleykum",ToastrMessageType.Success,ToastrPosition.BottomRight);
-    // this.toastr.message("Hasan Helvali","Selamun Aleykum",ToastrMessageType.Info,ToastrPosition.TopCenter);
-    // this.toastr.message("Hasan Helvali","Selamun Aleykum",ToastrMessageType.Warning,ToastrPosition.TopLeft);
-    // this.toastr.message("Hasan Helvali","Selamun Aleykum",ToastrMessageType.Error,ToastrPosition.TopRight);
-    // this.toastr.message("Hasan Helvali","Selamun Aleykum",ToastrMessageType.Success,ToastrPosition.TopFullWidth);
-
-
-    this.toastr.message("Hasan Helvali","Selamun Aleykum",{messageType:ToastrMessageType.Info,position:ToastrPosition.TopCenter});
-    this.toastr.message("Hasan Helvali","Selamun Aleykum",{messageType:ToastrMessageType.Info,position:ToastrPosition.TopFullWidth});
-    this.toastr.message("Hasan Helvali","Selamun Aleykum",{messageType:ToastrMessageType.Info,position:ToastrPosition.TopLeft});
-    this.toastr.message("Hasan Helvali","Selamun Aleykum",{messageType:ToastrMessageType.Info,position:ToastrPosition.TopRight});
-    this.toastr.message("Hasan Helvali","Selamun Aleykum",{messageType:ToastrMessageType.Info,position:ToastrPosition.BottomCenter});
-    this.toastr.message("Hasan Helvali","Selamun Aleykum",{messageType:ToastrMessageType.Info,position:ToastrPosition.BottomFullWidth});
-    this.toastr.message("Hasan Helvali","Selamun Aleykum",{messageType:ToastrMessageType.Info,position:ToastrPosition.BottomLeft});
-    this.toastr.message("Hasan Helvali","Selamun Aleykum",{messageType:ToastrMessageType.Info,position:ToastrPosition.BottomRight});
-  }
 }
-
+$.get("https://localhost:7113/api/Products",data=>{console.log(data);
+});
 
 
